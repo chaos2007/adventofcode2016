ef2a1f5741f9d9201af0d8941747e9c717bd133a
Don Pollitz <chaos2007@gmail.com>
2016-12-20 21:07:28 -0600

    Submit part 2 of day2.

--- day02/class_Keypad.f90
+++ day02/class_Keypad.f90
@@ -1,19 +1,28 @@
 module class_Keypad
     implicit none
-    public :: Keypad, get_current_key, change_key, parse_line
+    public :: Keypad, get_current_key, change_key, parse_line, keys, keys2
     
-    integer, dimension(0:2,0:2) :: keys = reshape((/ 1, 2, 3, 4, 5, 6, 7, 8, 9 /), shape(keys))
+    character, dimension(0:2,0:2), target :: keys = reshape((/ '1', '2', '3', &
+                                                      &'4', '5', '6', &
+                                                      &'7', '8', '9' /), shape(keys))
+    character, dimension(0:4,0:4), target :: keys2 = reshape((/ '0', '0', '1', '0', '0', &
+                                                       &'0', '2', '3', '4', '5', &
+                                                       &'5', '6', '7', '8', '9', &
+                                                       &'0', 'A', 'B', 'C', '0', &
+                                                       &'0', '0', 'D', '0', '0' /), shape(keys2))
+    integer, dimension(1:2)  :: keys_dim = shape(keys)
 
     type Keypad
         integer :: current_x = 1
-        integer :: current_y = 1
+        integer :: current_y = 1 
+        character, pointer, dimension(:,:) :: key => keys
     end type Keypad
 
     contains
         function get_current_key(this) result(key)
             type(Keypad), intent(in) :: this
-            integer :: key
-            key = keys(this%current_x, this%current_y)
+            character :: key
+            key = this%key(this%current_x, this%current_y)
         end function get_current_key
 
         subroutine parse_line(this, line)
@@ -29,6 +38,10 @@ module class_Keypad
             type(Keypad), intent(inout) :: this
             character, intent(in) :: direction
             integer :: x, y
+            integer :: keys_size 
+            integer, dimension(1:2)  :: keys_dim
+            keys_dim = shape(this%key)
+            keys_size = keys_dim(1)
             x  = this%current_x
             y = this%current_y
             select case (direction)
@@ -43,7 +56,7 @@ module class_Keypad
                 case default
                     call exit()
              end select
-             if( x >= 0 .and. x <= 2 .and. y >= 0 .and. y <= 2) then
+             if( x >= 0 .and. y >= 0 .and. x < keys_size .and. y < keys_size .and. this%key(x, y) .ne. '0' ) then
                  this%current_x = x
                  this%current_y = y
              endif
--- day02/class_Keypad.fun
+++ day02/class_Keypad.fun
@@ -9,38 +9,54 @@ end teardown
 test test_default_key_is_5
     type(Keypad) :: k
     k = Keypad()
-    assert_equal(5, get_current_key(k))
+    assert_equal('5', get_current_key(k))
 end test 
 
 test test_basic_change_key
     type(Keypad) :: k
     k = Keypad()
     call change_key(k, 'U')
-    assert_equal(2, get_current_key(k))
+    assert_equal('2', get_current_key(k))
     call change_key(k, 'R')
-    assert_equal(3, get_current_key(k))
+    assert_equal('3', get_current_key(k))
 end test 
 
 test test_bounds_checking
     type(Keypad) :: k
     k = Keypad()
     call change_key(k, 'U')
-    assert_equal(2, get_current_key(k))
+    assert_equal('2', get_current_key(k))
     call change_key(k, 'U')
-    assert_equal(2, get_current_key(k))
+    assert_equal('2', get_current_key(k))
 end test 
+
+test test_parse_line
+    type(Keypad) :: k
+    k = Keypad()
+    call parse_line(k, "ULLLLLLLL")
+    assert_equal('1', get_current_key(k))
+    call parse_line(k, "RRDDD")
+    assert_equal('9', get_current_key(k))
+end test
 
 test test_example
     type(Keypad) :: k
     k = Keypad()
     call parse_line(k, "ULL")
-    assert_equal(1, get_current_key(k))
+    assert_equal('1', get_current_key(k))
     call parse_line(k, "RRDDD")
-    assert_equal(9, get_current_key(k))
+    assert_equal('9', get_current_key(k))
     call parse_line(k, "LURDL")
-    assert_equal(8, get_current_key(k))
+    assert_equal('8', get_current_key(k))
     call parse_line(k, "UUUUD")
-    assert_equal(5, get_current_key(k))
+    assert_equal('5', get_current_key(k))
+end test
+
+test test_part2
+    type(Keypad) :: k
+    k = Keypad(0, 2, keys2)
+    call parse_line(k, "U")
+    assert_equal('5', get_current_key(k))
 end test
 
 end test_suite 
