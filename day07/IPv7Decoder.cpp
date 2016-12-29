5d19a1dcd904956ac561aabdde9b7b8923879a8c
Don Pollitz <chaos2007@gmail.com>
2016-12-29 01:58:31 -0600

    Prob 1 working.

--- day07/IPv7Decoder.cpp
+++ day07/IPv7Decoder.cpp
@@ -1,17 +1,92 @@
 #include<gtest/gtest.h>
 #include<string>
+#include<cstdint>
+#include<iostream>
+#include<fstream>
 
 class IPv7Decoder {
+public:
+    IPv7Decoder() {};
+    ~IPv7Decoder() {};
 
-    public:
-        IPv7Decoder() {};
-        ~IPv7Decoder() {};
+    bool contains_bab(const std::string &str ) {
+        bool inside_hypernet = false;
+        bool abba = false;
+        for( int x = 0; x < str.length() - 2; x++ ) {
+            if( str[x] == '[' ) {
+                inside_hypernet = true;
+            }
+            if( str[x] == ']' ) {
+                inside_hypernet = false;
+            }
+            if( !inside_hypernet ) {
+                if( check_for_bab((uint8_t *)&(str.c_str()[x])) ) {
+                    auto y = (str.substr(x, 1) + str.substr(x+1, 1) + str.substr(x+2,1));
+                    if( str.find(y)  != std::string::npos ) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return abba;
+    }
 
-        bool valid_address(const std::string &str) {return false;};
+    bool contains_abba(const std::string &str) {
+        bool inside_hypernet = false;
+        bool abba = false;
+        for( int x = 0; x < str.length() - 3; x++ ) {
+            if( str[x] == '[' ) {
+                inside_hypernet = true;
+            }
+            if( str[x] == ']' ) {
+                inside_hypernet = false;
+            }
+            if( check_for_abba((uint8_t *)&(str.c_str()[x]))) {
+                if( inside_hypernet ) {
+                    return false;
+                } else {
+                    abba = true;
+                }
+            }
+        }
+        return abba;
+    }
 
-    private:
+    uint32_t count_valid_babs(const std::string &str) {
+        std::string line;
+        std::ifstream myfile(str.c_str());
+        uint32_t count = 0;
+        if( myfile.is_open() ) {
+            while( getline( myfile, line ) ) {
+                if( contains_bab(line) ) count++;
+            }
+        }
+        return count;
+    }
 
+    uint32_t count_valid_abbas(const std::string &str) {
+        std::string line;
+        std::ifstream myfile(str.c_str());
+        uint32_t count = 0;
+        if( myfile.is_open() ) {
+            while( getline( myfile, line ) ) {
+                if( contains_abba(line) ) count++;
+            }
+        }
+        return count;
+    }
 
+private:
+    bool check_for_abba(const uint8_t* addr) {
+        return (addr[0] == addr[3] &&
+                addr[1] == addr[2] &&
+                addr[0] != addr[1]);
+    }
+
+    bool check_for_bab(const uint8_t* addr) {
+        return (addr[0] == addr[2] &&
+                addr[0] != addr[1] );
+    }
 };
 
 
@@ -23,22 +98,24 @@ TEST(Decoder, constructor) {
 
 TEST(Decoder, example_1) {
     IPv7Decoder decoder;
-    ASSERT_TRUE(decoder.valid_address("abba[mnop]qrst"));
+    EXPECT_TRUE(decoder.contains_abba("abba[mnop]qrst"));
+    EXPECT_FALSE(decoder.contains_abba("abcd[bddb]xyyx"));
+    EXPECT_FALSE(decoder.contains_abba("aaaa[qwer]tyui"));
+    EXPECT_TRUE(decoder.contains_abba("ioxxoj[asdfgh]zxcvbn"));
+    EXPECT_EQ(2, decoder.count_valid_abbas("example_1.txt"));
 }
-/*
+
 TEST(Decoder, example_2) {
     IPv7Decoder decoder;
-    ASSERT_FALSE(decoder.valid_address("abcd[bddb]xyyx"));
+    EXPECT_TRUE(decoder.contains_bab("aba[bab]xyz"));
+    EXPECT_FALSE(decoder.contains_bab("xyx[xyx]xyx"));
+    EXPECT_TRUE(decoder.contains_bab("aaa[kek]eke"));
+    EXPECT_TRUE(decoder.contains_bab("zazbz[bzb]cdb"));
+    EXPECT_EQ(3, decoder.count_valid_babs("example_2.txt"));
 }
 
-TEST(Decoder, example_3) {
+TEST(Decoder, problem_1) {
     IPv7Decoder decoder;
-    ASSERT_FALSE(decoder.valid_address("aaaa[qwer]tyui"));
+    EXPECT_EQ(118, decoder.count_valid_abbas("prob_1.txt"));
 }
 
-TEST(Decoder, example_4) {
-    IPv7Decoder decoder;
-    ASSERT_TRUE(decoder.valid_address("ioxxoj[asdfgh]zxcvbn"));
-}
-
-*/
